aceaf5b0bb1062f1b1a735d63f442dc05f149886
Ander Garmendia <agarmendia@countercraft.eu>
2018-05-04 20:09:13 +0200

    Refactor EstropadakParser to allow registering new parsers

--- estropadakparser/tests/test_estropadakparser.py
+++ estropadakparser/tests/test_estropadakparser.py
@@ -1,7 +1,7 @@
 import pytest
 import urllib.request
 import re
-from estropadakparser.estropadakparser import EstropadakParser, EuskotrenEgutegiaParser
+from estropadakparser.estropadakparser import EstropadakParser, EuskotrenEgutegiaParser, ActParser
 from estropadakparser.estropada.estropada import Estropada
 
 
@@ -17,6 +17,28 @@ from estropadakparser.estropada.estropada import Estropada
 #                         'ver.php?id=es&r=1395519778')
 #        euskotrenRaceResult = urllib.request.urlopen(euskotren_url)
 #        self.euskotrenRaceHtml = euskotrenRaceResult.read()
+
+def test_register_parser():
+    act_url = ('http://www.ligasanmiguel.com/resultados/'
+                'ver.php?id=eu&r=1365765288')
+    EstropadakParser.register('actt', ActParser)
+    estropada = EstropadakParser('actt').parse(act_url)
+    assert str(estropada) == 'Bandera Euskadi Basque Country (2013-06-16 12:00)'
+    assert estropada.izena == 'Bandera Euskadi Basque Country'
+    assert len(estropada.sailkapena) == 12
+    assert estropada.urla == act_url
+    for taldea in estropada.sailkapena:
+        assert type(taldea.posizioa) == int
+        assert taldea.posizioa in range(1, 13)
+        assert type(taldea.puntuazioa) == int
+        assert taldea.puntuazioa in range(1, 13)
+        assert type(taldea.kalea) == int
+        assert taldea.kalea in range(1, 5)
+        assert type(taldea.tanda) == int
+        assert taldea.tanda in range(1, 4)
+        for ziab in taldea.ziabogak:
+            assert re.match('\d{2}:\d{2}', ziab) is not None
+        assert re.match('\d{2}:\d{2},\d{2}', taldea.denbora) is not None
 
 def test_parse_act():
     act_url = ('http://www.ligasanmiguel.com/resultados/'
