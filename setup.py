7703280474cf77c11de92082ee51ffd0bf7841d9
Ander Garmendia <kelertxiki@gmail.com>
2019-06-30 22:29:38 +0200

    Fix exported packages

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='EstropadakParser',
-      version='0.1.6',
+      version='0.1.7',
       description='Python module for parsing Estropadak',
       author='Ander Garmendia',
       author_email='kelertxiki@gmail.com',
       url='http://www.estropadak.net',
-      packages=['estropadakparser', 'estropadakparser.estropada'],
+      packages=['estropadakparser', 'estropadakparser.estropada', 'estropadakparser.parsers', 'estropadakparser.egutegia_parsers'],
       package_data={'estropadakparser': ['data/*.txt']}
      )
