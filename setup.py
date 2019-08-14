42db35812da5a05a4ee64134fcff826b6996cec8
Ander Garmendia <kelertxiki@gmail.com>
2019-08-14 23:42:40 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='EstropadakParser',
-      version='0.1.8',
+      version='0.1.9',
       description='Python module for parsing Estropadak',
       author='Ander Garmendia',
       author_email='kelertxiki@gmail.com',
