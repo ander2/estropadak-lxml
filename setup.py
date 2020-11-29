6d3c06daf0dcc3e9339b641b2db364b55fb05aed
Ander Garmendia <kelertxiki@gmail.com>
2020-11-29 17:22:35 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,11 @@ setup(name='EstropadakParser',
       description='Python module for parsing Estropadak',
       author='Ander Garmendia',
       author_email='kelertxiki@gmail.com',
-      url='http://www.estropadak.net',
+      url='http://www.estropadak.eus',
       packages=['estropadakparser', 'estropadakparser.estropada', 'estropadakparser.parsers', 'estropadakparser.egutegia_parsers'],
-      package_data={'estropadakparser': ['data/*.txt']}
+      package_data={'estropadakparser': ['data/*.txt']},
+      install_requires=[
+          'lxml',
+          'cssselect'
+      ]
      )
