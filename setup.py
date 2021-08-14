45e791c1f60736fef55df51ca9c682a785c6c921
Ander Garmendia <kelertxiki@gmail.com>
2021-08-14 09:53:15 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@
 
 from distutils.core import setup
 
-setup(name='EstropadakParser',
-      version='0.1.14',
-      description='Python module for parsing Estropadak',
-      author='Ander Garmendia',
-      author_email='kelertxiki@gmail.com',
-      url='http://www.estropadak.eus',
-      packages=['estropadakparser', 'estropadakparser.estropada', 'estropadakparser.parsers', 'estropadakparser.egutegia_parsers'],
-      package_data={'estropadakparser': ['data/*.txt']},
-      install_requires=[
-          'lxml',
-          'cssselect'
-      ]
-     )
+setup(
+    name='EstropadakParser',
+    version='0.1.15',
+    description='Python module for parsing Estropadak',
+    author='Ander Garmendia',
+    author_email='kelertxiki@gmail.com',
+    url='http://www.estropadak.eus',
+    packages=['estropadakparser', 'estropadakparser.estropada', 'estropadakparser.parsers', 'estropadakparser.egutegia_parsers'],
+    package_data={'estropadakparser': ['data/*.txt']},
+    install_requires=[
+        'lxml',
+        'cssselect'
+    ]
+)
